aeb897e631205ffcd8eafffa194c0363b7a7a46e
Joan Segura <joan@playbusiness.mx>
2015-04-01 18:49:15 -0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+UdemyApp::Application.config.secret_key_base = '1aafb0f26d6f62bb84d27813ae2b097f7404e6cf683059c03a5f1a94b39b604bae757c2ae3c6bd6f5d0aa2dea0029f13d2fb5096573dd16890ce490628f21ff1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+UdemyApp::Application.config.session_store :cookie_store, key: '_udemy_app_session'
